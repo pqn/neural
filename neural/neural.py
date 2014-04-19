c7de15b7f5d8e692d99ec768d8c6ef1f381fe05a
Prem Nair <prem.q.nair@gmail.com>
2014-04-18 23:53:58 -0700

    Getting there!

--- neural/neural.py
+++ neural/neural.py
@@ -24,29 +24,59 @@ class NeuralNetwork:
         else:
             eye = np.eye(self.__num_labels)
             self.__y = np.matrix([eye[example[1]] for example in training_set])
-        self.__m = self.__X[0].shape[0]
-        self.__input_layer_size = self.__X[0].shape[1]
+        self.__m = self.__X.shape[0]
+        self.__input_layer_size = self.__X.shape[1]
         self.__sizes = [self.__input_layer_size]
         self.__sizes.extend(self.__hidden_layers)
         self.__sizes.append(self.__num_labels)
         initial_theta = []
         for count in range(len(self.__sizes) - 1):
             epsilon = np.sqrt(6) / np.sqrt(self.__sizes[count]+self.__sizes[count+1])
-            initial_theta.append(np.random.rand((self.__sizes[count] + 1),self.__sizes[count+1])*2*epsilon-epsilon)
+            initial_theta.append(np.random.rand(self.__sizes[count+1],self.__sizes[count]+1)*2*epsilon-epsilon)
         initial_theta = self.__unroll(initial_theta)
-        # self.__thetas = self.__roll(fmin_ncg(self.__cost, np.zeros(param_size), self.__costGrad))
+        self.__thetas = self.__roll(fmin_ncg(self.__cost_function, initial_theta, self.__cost_grad_function, maxiter=iterations))
 
-    def __cost(self, params):
-        """Computes cost function."""
+    def __cost_function(self, params):
+        return self.__cost(params, True)
+
+    def __cost_grad_function(self, params):
+        return self.__cost(params, False)
+
+    def __cost(self, params, just_cost):
+        """Computes cost function and derivative."""
         params = self.__roll(params)
-        a = np.concatenate((np.ones((self.__m, 1)), self.__X), axis=1)
-        calculated_a = [a]
-        for theta in params:
-            a = self.sigmoid(a*theta.transpose())
-            calculated_a.add(a)
+        a = np.concatenate((np.ones((self.__m, 1)), self.__X), axis=1) # This is a1
+        calculated_a = [a] # a1 is at index 0, a_n is at index n-1
+        calculated_z = [0] # There is no z1, z_n is at index n-1
+        for i, theta in enumerate(params): # calculated_a now contains a1, a2, a3 if there was only one hidden layer (two theta matrices)
+            z = calculated_a[-1] * theta.transpose() # z_n = a_n-1 * Theta_n-1'
+            calculated_z.append(z) # Save the new z_n
+            a = self.sigmoid(z) # a_n = sigmoid(z_n)
+            if i != len(params) - 1: # Don't append extra ones for the output layer
+                a = np.concatenate((np.ones((a.shape[0], 1)), a), axis=1) # Append the extra column of ones for all other layers
+            calculated_a.append(a) # Save the new a
 
-    def __cost_grad(self, params):
-        """Computes cost function derivative."""
+        J = np.sum(-np.multiply(self.__y, np.log(a[-1]))-np.multiply(1-self.__y, np.log(1-a[-1])))/self.__m; # Calculate cost
+        if self.__lambda != 0: # If we're using regularization...
+            J += np.sum([np.power(theta[:,1:], 2) for theta in params])*self.__lambda/(2.0*self.__m) # ...add it from all theta matrices
+
+        if just_cost:
+            return J
+
+        reversed_d = []
+        reversed_theta_grad = []
+        for i in range(len(params)): # For once per theta matrix...
+            if i == 0: # ...if it's the first one...
+                d = a[-1] - self.__y # ...initialize the error...
+            else: # ...otherwise d_n-1 = d_n * Theta_n-1[missing ones] .* sigmoid(z_n-1)
+                d = np.multiply(reversed_d[-1]*params[-i][:,1:], self.sigmoid_grad(calculated_z[-1-i])) # With i=1/1 hidden layer we're getting Theta2 at index -1, and z2 at index -2
+            reversed_d.append(d)
+            theta_grad = reversed_d[-1].transpose() * calculated_a[-i-2] / self.__m
+            if self.__lambda != 0:
+                theta_grad += self.__lambda * 1.0 / self.__m * np.concatenate((np.zeros((params[-i].shape[0], 1)), params[-i][:,1:]), axis=1) # regularization
+            reversed_theta_grad.append(theta_grad)
+        theta_grad = self.__unroll(reversed(reversed_theta_grad))
+        return theta_grad
 
     def __roll(self, unrolled):
         """Converts parameter array back into matrices."""
@@ -70,3 +100,6 @@ class NeuralNetwork:
 
     def sigmoid_grad(self, z):
         return np.multiply(self.sigmoid(z), 1-self.sigmoid(z))
+
+    def optimized_thetas(self):
+        print(self.__thetas)
